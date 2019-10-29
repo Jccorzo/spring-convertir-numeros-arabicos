10a3249775bc3bfcc052c019f8de0d868ed4b8c3
Camilo Corzo <camilo@MacBook-Air-de-Camilo.local>
2019-10-29 13:32:33 -0500

    Creación del proyecto

---
+++ src/main/java/com/springboot/ArabigosARomanos/ArabigosARomanosApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ArabigosARomanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArabigosARomanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArabigosARomanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/ArabigosARomanos/ArabigosARomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ArabigosARomanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArabigosARomanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
