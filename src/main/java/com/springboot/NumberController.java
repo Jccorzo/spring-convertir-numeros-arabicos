c9111802cf0b12907a719e8fb3c6eb28db3ba7a3
camilo <camilo@localhost>
2019-10-30 09:52:19 -0500

    creacion del metodo de validacion del numero

--- src/main/java/com/springboot/NumberController.java
+++ src/main/java/com/springboot/NumberController.java
@@ -1,11 +1,13 @@
 package com.springboot;
 
-import static com.springboot.Number.transformNumberToRoman;
+import static com.springboot.NumberOperation.transformNumberToRoman;
+import static com.springboot.NumberOperation.validateNumber;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,14 +25,27 @@ public class NumberController {
 	   public List<Number> consultTransformedNumbers() {
 	    return repository.findAll();
 	  }
+	
+	@GetMapping("/getRequestsNumber")
+	   public Long getRequestsNumber() {
+	    return repository.count();
+	  }
 	 
 
 	@PostMapping("/getNumber")
-		public String newNumber(@RequestParam(value = "number") String consultedNumber) {
-		String responsedNumber = transformNumberToRoman(Integer.parseInt(consultedNumber));
-		Number objectToSave = new Number(consultedNumber, responsedNumber);
-		repository.save(objectToSave);
+		public String transformNumber(@RequestParam(value = "number") String requestedNumber){
+		String responsedNumber = "Solicite un número entero mayor a 0 y menor a 3999";
+		if (validateNumber(requestedNumber)) {
+			responsedNumber = transformNumberToRoman(Integer.parseInt(requestedNumber));
+			Number objectToSave = new Number(requestedNumber, responsedNumber);
+			repository.save(objectToSave);
+		} 
 		return responsedNumber;
 	  }
+	
+	@PostMapping("/saveNumber")
+	public Number saveNumber (@RequestBody Number newNumber) {
+		return repository.save(newNumber);
+	}
 	
 }
---
+++ src/main/java/com/springboot/NumberOperation.java
@@ -0,0 +1,46 @@
+package com.springboot;
+
+import java.util.TreeMap;
+
+public class NumberOperation {
+	
+	private NumberOperation() {}
+	
+    private static final TreeMap<Integer, String> map = new TreeMap<>();
+
+    static {
+
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+
+    }
+
+    public static String transformNumberToRoman(int requestNumber) {
+        int consultedNumber =  map.floorKey(requestNumber);
+        if ( requestNumber == consultedNumber ) {
+            return map.get(requestNumber);
+        }
+        return map.get(consultedNumber) + transformNumberToRoman(requestNumber-consultedNumber);
+    }
+
+    public static boolean validateNumber(String requestedNumber) {
+    	boolean numberIsValid = true; 
+    	int number = Integer.parseInt(requestedNumber);
+    	if(number <= 0 || number > 3999) {
+    		numberIsValid = false;
+    	}
+		return numberIsValid;
+    	
+    }
+}
